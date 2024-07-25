f1852c16a6f23185d087fb6b219fefd0148cbd38
rohit verma <rohithverma.dev@gmail.com>
2024-07-26 00:41:45 +0530

    nnsjs

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -16,11 +16,11 @@ const Header = () => {
             <Link className="login_btn" to="/dashboard">Login</Link>
             <a href="#HOME" className="logo"> <span>D</span>ynamic <span>P</span>ortfolio.</a>
             <nav ref={navRef} className="navbar">
-                <a href="#HOME" onClick={ () => {setActiveNav("#HOME"); showNavbar();  } } className={(activeNav === "#HOME") ? "active" : ""} style={{ '--i': 1 }} >Home</a>
-                <a href="#HOME-SKILLS" onClick={() => {setActiveNav("#HOME-SKILLS"); showNavbar(); }} className={(activeNav === "#HOME-SKILLS") ? "active" : ""} style={{ '--i': 2 }}>Skills</a>
-                <a href="#TIMELINE-SECTION" onClick={()=>{ setActiveNav("#TIMELINE-SECTION"); showNavbar(); }}  className={(activeNav === "#TIMELINE-SECTION") ? "active" : ""}  style={{ '--i': 3 }}>Timeline</a>
-                <a href="#PROJECTS-SECTION" onClick={() => {setActiveNav("#PROJECTS-SECTION"); showNavbar() ;  }  } className={(activeNav === "#PROJECTS-SECTION") ? "active" : ""} style={{ '--i': 4 }}>Projects</a>
-                <a href="#CONTACT-PAGE" onClick={() => {setActiveNav("#CONTACT-PAGE"); showNavbar();  } } className={(activeNav === "#CONTACT-PAGE") ? "active" : ""} style={{ '--i': 5 }}>Contact</a>
+                <a href="#HOME" onClick={ () => {setActiveNav("#HOME"); showNavbar();  } } className={activeNav === "#HOME" ? "active" : ""} style={{ '--i': 1 }} >Home</a>
+                <a href="#HOME-SKILLS" onClick={() => {setActiveNav("#HOME-SKILLS"); showNavbar(); }} className={activeNav === "#HOME-SKILLS" ? "active" : ""} style={{ '--i': 2 }}>Skills</a>
+                <a href="#TIMELINE-SECTION" onClick={()=>{ setActiveNav("#TIMELINE-SECTION"); showNavbar(); }} style={{ '--i': 3 }}>Timeline</a>
+                <a href="#PROJECTS-SECTION" onClick={() => {setActiveNav("#PROJECTS-SECTION"); showNavbar() ;  }  } className={activeNav === "#PROJECTS-SECTION" ? "active" : ""} style={{ '--i': 4 }}>Projects</a>
+                <a href="#CONTACT-PAGE" onClick={() => {setActiveNav("#CONTACT-PAGE"); showNavbar();  } } className={activeNav === "#CONTACT-PAGE" ? "active" : ""} style={{ '--i': 5 }}>Contact</a>
                 <button
                     className="nav-btn nav-close-btn"
                     onClick={showNavbar}>
