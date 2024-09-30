2c769f73f85edad6dabc68bc27e7b1f9bd46dfca
rohit verma <rohithverma.dev@gmail.com>
2024-09-30 10:35:07 +0530

    loader added in home page

---
+++ src/components/Loader/Loader.jsx
@@ -0,0 +1,39 @@
+import React from 'react';
+
+const Loader = ({ color = '#660553' }) => {
+  return (
+    <div style={{
+      height: '100vh',
+      display: 'flex',
+      alignItems: 'center',
+      justifyContent: 'center',
+      backgroundColor:'rgb(8, 218, 249 , 0.5)',
+      zIndex:1,
+      position:'relative',
+    }}>
+      <div style={{ transform: 'scale(4)', width: 'fit-content', height: 'fit-content' }}>
+        <div
+          style={{
+            border: `3px solid ${color}`,
+            borderTopColor: "transparent",
+            borderRadius: '50%',
+            width: '2rem',
+            height: '2rem',
+            animation: 'spin 0.8s linear infinite'
+          }}
+        />
+      </div>
+      <style>
+        {`
+          @keyframes spin {
+            0% { transform: rotate(0deg); }
+            100% { transform: rotate(360deg); }
+          }
+        `}
+      </style>
+    </div>
+    
+  );
+};
+
+export default Loader;
