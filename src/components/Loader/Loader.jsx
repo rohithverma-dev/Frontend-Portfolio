3df00f63d9a4a97e0d3c4303741259e651d67c0f
ROHIT VERMA <167316301+rohithverma-dev@users.noreply.github.com>
2024-10-01 12:10:57 +0530

    Update Loader.jsx

--- src/components/Loader/Loader.jsx
+++ src/components/Loader/Loader.jsx
@@ -4,12 +4,13 @@ const Loader = ({ color = '#660553' }) => {
   return (
     <div style={{
       height: '100vh',
+      width:'100vw'
       display: 'flex',
       alignItems: 'center',
       justifyContent: 'center',
       backgroundColor:'rgb(8, 218, 249 , 0.5)',
-      zIndex:1,
-      position:'relative',
+      zIndex: 1000000 ,
+      position:'fixed',
     }}>
       <div style={{ transform: 'scale(4)', width: 'fit-content', height: 'fit-content' }}>
         <div
